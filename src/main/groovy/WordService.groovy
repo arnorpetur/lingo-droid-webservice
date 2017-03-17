c08c13c29f2565f8c37475fa34d1add61f3b5245
arnorpetur <apm2@hi.is>
2017-03-17 00:35:35 +0000

    full

--- src/main/groovy/WordService.groovy
+++ src/main/groovy/WordService.groovy
@@ -40,7 +40,7 @@ class WordService {
         wordDbCommands.find(icelandic).map { GroovyRowResult dbRow ->
             return new Word(
                     icelandic,
-                    dbrow.english,
+                    dbRow.english,
                     dbRow.difficulty
             )
         }
